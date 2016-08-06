90ab57e1fd8196ea9dd530815d5a8f4e2857c079
Thor Kell <thor@tide-pool.ca>
2016-08-06 18:46:07 -0400

    tests for select_work

--- tests/test_work_utils.py
+++ tests/test_work_utils.py
@@ -6,6 +6,7 @@ from mock import MagicMock
 from mock import call
 
 from work_utils import do_work
+from work_utils import select_work
 
 import tag_utils
 
@@ -16,6 +17,7 @@ def test_do_work_rename():
     os.rename.assert_called_with('old_path', 'new_path')
 
 def test_do_work_retag():
+    # Note that this implicity tests tag_utils
     retag_tasks = [('retag', 'path', 'the_artist', 'the_title')]
     tag_utils.set_tag = MagicMock()
     try:
@@ -26,3 +28,17 @@ def test_do_work_retag():
     c2 = call('path', 'title', 'the_title')
     tag_utils.set_tag.assert_has_calls([c1, c2])
 
+
+#select_work(filepath, tag_artist, tag_title, file_artist, file_title, new_name_from_file, new_name_from_tag)
+def test_select_work_rename():
+    res = select_work('path', 'artist', 'title', 'artist', 'title', 'new_file_name', 'new_tag_name')
+    assert(res == [('rename', 'path', 'new_file_name')])
+
+def test_select_work_tag_rename():
+    res = select_work('path', 'artist', 'title', 'tag_artist', 'tag_title', 'new_file_name', 'new_tag_name')
+    assert(res == [('rename', 'path', 'new_tag_name')])
+
+def test_select_work_rename_and_move():
+    res = select_work('path', None, None, 'artist', 'title', 'new_file_name', 'new_tag_name')
+    print res
+    assert(res == [('retag', 'path', 'artist', 'title'), ('rename', 'path', 'new_file_name')])
--- work_utils.py
+++ work_utils.py
@@ -38,10 +38,12 @@ def select_work(filepath, tag_artist, tag_title, file_artist, file_title, new_na
     # if they are the same, we just need to rename the file
     if tag_artist == file_artist and tag_title == file_title:
         new_file_name = new_name_from_file
-        task = ('rename', new_file_name)
+        folder_path = os.path.join(os.path.split(filepath)[0:-1])[0]
+        new_filepath = os.path.join(folder_path, new_file_name)
+        task = ('rename', filepath, new_file_name)
         work.append(task)
     else:
-        # If there are tags, we rename
+        # If there are tags, we rename based on the tag:w
         if tag_artist and tag_title:
             extension = filepath.split('.')[-1]
             new_file_name = new_name_from_tag
@@ -56,7 +58,7 @@ def select_work(filepath, tag_artist, tag_title, file_artist, file_title, new_na
 
             new_file_name = new_name_from_file
             folder_path = os.path.join(os.path.split(filepath)[0:-1])[0]
-            new_filepath = os.path.join(folder_path, new_file_name)
+            new_file_path = os.path.join(folder_path, new_file_name)
             task = ('rename', filepath, new_file_path)
             work.append(task)
 
