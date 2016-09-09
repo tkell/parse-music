f7e46ce80a68a4c387f30ff4437b659c6e6cc4d3
Thor Kell <thor@tide-pool.ca>
2016-09-08 21:09:49 -0400

    cut boomkat

--- parser.py
+++ parser.py
@@ -43,10 +43,7 @@ def build_parsers():
     parsers = []
     # name, album_regex, file_regex
     ## Will we need a 'single regex'?  Will depend on the store, I betcha =\
-    # need to add Bandcamp and Boomkat, in that order.
-    # boomkat is trouble, as it is just the album name, with underscores.  
-    # Do I need to, ugh, make a list of filters that these go through?  
-    # an underscore-to-spaces too, for example?
+    # boomkat is trouble, and should be avoided
 
     data = [
             # www.bleep.com
@@ -60,12 +57,6 @@ def build_parsers():
               'bandcamp',
               r'NO EXAMPLES YET', 
               r'(?P<artist>.+?) - (?P<title>.+?)\.(?P<extension>.+?)'
-            ),
-            # www.boomkat.com
-            (
-              'boomkat',
-              r'(?P<album_title>.+?)',
-              r'NO EXAMPLES YET', 
             ),
         ]
     for name, album_regex_string, file_regex_string in data:
--- run.py
+++ run.py
@@ -9,5 +9,5 @@ from parse_singles import parse_singles
 from parse_albums import parse_albums
 
 if __name__ == '__main__':
-    parse_singles('/Users/thor/Desktop/unparsed/singles', '/Users/thor/Desktop/parsed')
-    # parse_albums('/Users/thor/Desktop/unparsed', '/Users/thor/Desktop/parsed')
+    parse_singles('/Users/thor/Desktop/unparsed/singles', '/Users/thor/Desktop/parsed/singles')
+    parse_albums('/Users/thor/Desktop/unparsed', '/Users/thor/Desktop/parsed')
