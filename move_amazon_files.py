27d854916bf3f4b2cf0d689b2e45eefe97ff114f
Thor Kell <thor@tide-pool.ca>
2019-09-21 20:59:48 -0400

    update amazon preprocessor

--- move_amazon_files.py
+++ move_amazon_files.py
@@ -1,9 +1,10 @@
 ## Deals with amazon's stupid directories, prepends them with AMAZON
-## This has been tweaked to only work for singles!
+## This has been tweaked to only work for singles, and rips album-tracks out to be singles!
 ## You'll need to hack it to work for albums, somehow.
 
 import os
 import shutil
+
 
 def run_amazon(path):
     files = os.listdir(path)
@@ -13,11 +14,15 @@ def run_amazon(path):
         for path_tuple in potential_files:
             filepath = path_tuple[0]
             if path_tuple[-1]:
-                filename = path_tuple[-1][0]
-                if '.mp3' in filename or '.flac' in filename:
-                    artist = path_tuple[0].split(os.path.sep)[-2]
-                    new_filename = 'AMAZON ' + artist + " - " + filename
-                    shutil.move(filepath + os.path.sep + filename, path + os.path.sep + new_filename)
+                filenames = path_tuple[-1]
+                for filename in filenames:
+                    if '.mp3' in filename or '.flac' in filename:
+                        artist = path_tuple[0].split(os.path.sep)[-2]
+                        new_filename = 'AMAZON ' + artist + " - " + filename
+                        shutil.move(
+                            filepath + os.path.sep + filename,
+                            path + os.path.sep + new_filename,
+                        )
+
 
 run_amazon('/Users/thor/Desktop/unparsed/singles')
-
